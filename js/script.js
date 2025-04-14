40a96626f58baf877e59307a7f98a2e5d0197720
Roland Bittencourt <lands.bitt@gamil.com>
2025-04-14 11:16:41 -0300

    FIT: AOS

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-
-// turns the menu hambuguer into a x
-
+// Menu hamburguer
 const menuHamburguer = document.querySelector('.menu-hamburguer');
 menuHamburguer.addEventListener('click', () => {
     toggleMenu();
 });
 
-function toggleMenu(){
-    const nav = document.querySelector('.nav-responsive')
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
     menuHamburguer.classList.toggle('change');
 
-    if (menuHamburguer.classList.contains('change')){
+    if (menuHamburguer.classList.contains('change')) {
         nav.style.display = 'block';
-    } else{
+    } else {
         nav.style.display = 'none';
     }
-    
-
 }
+
+// Inicializa o AOS
+AOS.init({
+    duration: 800, // Duração da animação (800ms para suavidade)
+    once: true,    // Anima apenas uma vez ao rolar
+    easing: 'ease-in-out', // Suavização natural
+    offset: 100,   // Inicia 100px antes do elemento entrar na tela
+});
